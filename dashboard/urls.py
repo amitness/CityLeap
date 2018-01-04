70362c1e3bbf975ae2a9accdff88d156075f408a
Amit <camitiw@gmail.com>
2018-01-04 22:41:25 +0545

    Add broadcast mechanism

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -3,5 +3,6 @@ from dashboard import views
 
 urlpatterns = [
     path('dashboard', views.dashboard, name='dashboard'),
-    path('complain', views.complain, name='complain')
+    path('complain', views.complain, name='complain'),
+    path('broadcast', views.broadcast, name='broadcast')
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,5 +1,7 @@
 # from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
+from decouple import config
+import requests
 
 
 def dashboard(request):
@@ -11,10 +13,22 @@ def complain(request):
     longitude = request.GET['lon']
     text = request.GET['text']
     category = request.GET['category']
-    # return HttpResponse(request.GET.values())
+    # TODO: Store in DB model
     result = {
         "messages": [
             {"text": "{}: {}: {}: {}".format(latitude, longitude, text, category)}
         ]
     }
     return JsonResponse(result)
+
+
+def broadcast(request):
+    bot_id = config('BOT_ID')
+    chatfuel_token = config('CHATFUEL_TOKEN')
+    user_id = '1813643665334588'
+    block_name = 'broadcast'
+    message = 'Your complain has been forwarded to the concerned authority.'
+    endpoint = "https://api.chatfuel.com/bots/{}/users/{}/send?chatfuel_token={}&chatfuel_block_name={}&message={}" \
+        .format(bot_id, user_id, chatfuel_token, block_name, message)
+    r = requests.post(endpoint)
+    return HttpResponse(r.status_code)
