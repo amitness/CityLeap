8e6b15189d7ae1fd485c4b5ae41ad63febfcb4a3
Amit <camitiw@gmail.com>
2018-01-05 05:27:43 +0545

    Show more log errors

--- CityLeap/settings.py
+++ CityLeap/settings.py
@@ -128,3 +128,18 @@ STATIC_URL = '/static/'
 # )
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': os.getenv('DJANGO_LOG_LEVEL', 'ERROR'),
+        },
+    },
+}
