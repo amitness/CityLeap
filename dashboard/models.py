dfccb759ef4711b1230380f7c3f3d5dd3e8e2723
Amit <camitiw@gmail.com>
2018-01-04 23:29:47 +0545

    Add Complain model

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,14 +1,10 @@
 from django.db import models
 
-# class Person(models.Model):
-#     messenger_id = models.CharField(max_length=50)
-#     name = models.CharField(max_length=50)
-#
-#
-# class Complain(models.Model):
-#     text = models.CharField(max_length=100)
-#     latitude = models.CharField(max_length=50)
-#     longitude = models.CharField(max_length=50)
-#     upvotes = models.IntegerField(default=True, null=True)
-#     category = models.CharField(max_length=50)
-#     person = models.ForeignKey(Person, on_delete='cascade')
+
+class Complain(models.Model):
+    text = models.CharField(max_length=100)
+    latitude = models.CharField(max_length=50)
+    longitude = models.CharField(max_length=50)
+    upvote_count = models.IntegerField(default=True, null=True)
+    category = models.CharField(max_length=50)
+    user_id = models.CharField(max_length=50)
