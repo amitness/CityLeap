0807c1538ea4d8fa0ef03d81f15182f23b3801a4
Amit <camitiw@gmail.com>
2018-01-04 23:38:42 +0545

    Add complain model

--- dashboard/models.py
+++ dashboard/models.py
@@ -5,6 +5,9 @@ class Complain(models.Model):
     text = models.CharField(max_length=100)
     latitude = models.CharField(max_length=50)
     longitude = models.CharField(max_length=50)
-    upvote_count = models.IntegerField(default=True, null=True)
+    upvote_count = models.IntegerField(default=1, null=True)
     category = models.CharField(max_length=50)
     user_id = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.text
